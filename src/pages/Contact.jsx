057064b3944634743b7e3fc61114d7b75161b53f
OtaraAlex <mbechealex61@gmail.com>
2023-11-16 17:48:17 +0300

    feat: configure emailjs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,28 +1,65 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  const [form, setForm] = useState({ name: '', email: '', message: '' })
-  const [isLoading, setIsLoading] = useState(false)
+  const formRef = useRef(null);
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = () => {};
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Alex Otara",
+          from_email: form.email,
+          to_email: "otarambeche@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+
+        // alert('Your message has been sent!');
+        // hide an alert after 5 seconds
+        setForm({ name: "", email: "", message: "" });
+      })
+      .catch(() => {
+        setIsLoading(false);
+        console.log(error);
+        // alert('Something went wrong, please try again.');
+      });
+  };
+
   const handleFocus = () => {};
   const handleBlur = () => {};
 
   return (
-    <section className='relative flex lg:flex-row flex-col max-container'>
-      <div className='flex-1 min-w-[50%] flex flex-col'>
-        <h1 className='head-text'>Get in Touch</h1>
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
 
         <form
-          className='w-full flex flex-col gap-7 mt-14'
+          className="w-full flex flex-col gap-7 mt-14"
+          onAbort={handleSubmit}
         >
           <label className="text-black-500 font-semibold">
             Name
-            <input 
-              type='text'
-              name='name'
-              className='input'
-              placeholder='John Doe'
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="John Doe"
               required
               value={form.name}
               onChange={handleChange}
@@ -32,11 +69,11 @@ const Contact = () => {
           </label>
           <label className="text-black-500 font-semibold">
             Email
-            <input 
-              type='email'
-              name='email'
-              className='input'
-              placeholder='johndoe@gmail.com'
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="johndoe@gmail.com"
               required
               value={form.email}
               onChange={handleChange}
@@ -47,10 +84,10 @@ const Contact = () => {
           <label className="text-black-500 font-semibold">
             Your Message
             <textarea
-              name='message'
+              name="message"
               rows={4}
-              className='textarea'
-              placeholder='Let me know how I can help you!'
+              className="textarea"
+              placeholder="Let me know how I can help you!"
               required
               value={form.message}
               onChange={handleChange}
@@ -59,18 +96,18 @@ const Contact = () => {
             />
           </label>
           <button
-            type='submit'
-            className='btn'
+            type="submit"
+            className="btn"
             disabled={isLoading}
             onFocus={handleFocus}
             onBlur={handleBlur}
           >
-            {isLoading ? 'Sending...' : 'Send Message'}
+            {isLoading ? "Sending..." : "Send Message"}
           </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
