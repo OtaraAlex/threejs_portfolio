d37039ea485902a393952f9b5cf3e3a1a4daa251
OtaraAlex <mbechealex61@gmail.com>
2023-11-17 11:11:34 +0300

    feat: fix event handling function

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -61,13 +61,12 @@ const Contact = () => {
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
       {alert.show && <Alert {...alert} />}
-      <Alert {...alert} />
 
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
         <form
-          onAbort={handleSubmit}
+          onSubmit={handleSubmit}
           className="w-full flex flex-col gap-7 mt-14"
         >
           <label className="text-black-500 font-semibold">
